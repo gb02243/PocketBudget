2c1bd4ecc6615d2dba3943e96b9c84cc94bf9d74
griffin <gb02243@georgiasouthern.edu>
2020-04-12 20:46:22 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+//use path module
+const path = require('path');
+//use express module
+const express = require('express');
+//use hbs view engine
+const hbs = require('hbs');
+//use bodyParser middleware
+const bodyParser = require('body-parser');
+//use mysql database
+const mysql = require('mysql');
+const app = express();
+
+//create connection
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'hci',
+  password: 'hci2020',
+  database: 'pocketbudget'
+});
+
+//connect to database
+conn.connect((err) =>{
+  if(err) throw err;
+  console.log('Mysql Connected...');
+});
+
+//set views file
+app.set('views',path.join(__dirname,'views'));
+//set view engine
+app.set('view engine', 'hbs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//set public folder as static folder for static file
+app.use('/assets',express.static(__dirname + '/public'));
+
+//route for homepage
+app.get('/',(req, res) => {
+  res.render('homepage');
+});
+
+//server listening
+app.listen(8000, () => {
+  console.log('Server is running at port 8000');
+});
