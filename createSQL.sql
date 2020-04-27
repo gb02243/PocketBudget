a90bc2b293433b90614acfa2fcd526fc6a42fa68
Griffin Bryant <48259830+gb02243@users.noreply.github.com>
2020-04-27 09:08:39 -0400

    + added transaction table

--- createSQL.sql
+++ createSQL.sql
@@ -13,21 +13,30 @@ CREATE TABLE `users` (
   `state` varchar(45) NOT NULL,
   `zip` int(5) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `budget` (
-	`budget_id`			int(11)						NOT NULL	AUTO_INCREMENT,
-    `user_id`			int(11)						NOT NULL,
-	`amount_total`		double(11, 2)	DEFAULT 0	NOT NULL,
-    `bills_percent`		double(5, 1)	DEFAULT 0	NOT NULL,
-    `food_percent`		double(5, 1)	DEFAULT 0	NOT NULL,
-    `gas_percent`		double(5, 1)	DEFAULT 0	NOT NULL,
-    `savings_percent`	double(5, 1)	DEFAULT 0	NOT NULL,
-    `fun_percent`		double(5, 1)	DEFAULT 0	NOT NULL,
-    PRIMARY KEY (`budget_id`),
-    FOREIGN KEY (`user_id`) REFERENCES users(`user_id`)
 );
 
+CREATE TABLE `budget` (
+  `budget_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+	`amount_total` double(11, 2)	DEFAULT 0	NOT NULL,
+  `bills_percent` double(5, 1) DEFAULT 0 NOT NULL,
+  `food_percent` double(5, 1)	DEFAULT 0	NOT NULL,
+  `gas_percent` double(5, 1) DEFAULT 0 NOT NULL,
+  `savings_percent` double(5, 1) DEFAULT 0 NOT NULL,
+  `fun_percent` double(5, 1) DEFAULT 0 NOT NULL,
+  PRIMARY KEY (`budget_id`)
+);
+
+CREATE TABLE `transaction` (
+  `transaction_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `amount` double(11, 2) NOT NULL,
+  PRIMARY KEY (`transaction_id`)
+);
+
+ALTER TABLE `budget` ADD CONSTRAINT `budgetUserFK` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `transaction` ADD CONSTRAINT `transactionUserFK` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 insert into `users` (fullname, email, password, city, state, zip) values
 ('Brandon Derricho', 'bderricho@pocketbudget.com', 'derricho', 'Statesboro', 'GA', 30458),
