d84e58ce5fcc8d15c71610c3ae2aa5bb6e34215c
Griffin Bryant <48259830+gb02243@users.noreply.github.com>
2020-04-27 10:17:07 -0400

    + added category

--- createSQL.sql
+++ createSQL.sql
@@ -30,6 +30,7 @@ CREATE TABLE `budget` (
 CREATE TABLE `transaction` (
   `transaction_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
+  `category` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   `amount` double(11, 2) NOT NULL,
   PRIMARY KEY (`transaction_id`)
