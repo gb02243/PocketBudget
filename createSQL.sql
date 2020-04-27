a120ab0d7e0f3c2683c8cadf6fc63122e4fb1662
Griffin Bryant <48259830+gb02243@users.noreply.github.com>
2020-04-27 12:31:34 -0400

    + added dummy data

--- createSQL.sql
+++ createSQL.sql
@@ -18,7 +18,7 @@ CREATE TABLE `users` (
 CREATE TABLE `budget` (
   `budget_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
-	`amount_total` double(11, 2)	DEFAULT 0	NOT NULL,
+  `amount_total` double(11, 2)	DEFAULT 0	NOT NULL,
   `bills_percent` double(5, 1) DEFAULT 0 NOT NULL,
   `food_percent` double(5, 1)	DEFAULT 0	NOT NULL,
   `gas_percent` double(5, 1) DEFAULT 0 NOT NULL,
@@ -40,13 +40,30 @@ ALTER TABLE `budget` ADD CONSTRAINT `budgetUserFK` FOREIGN KEY (`user_id`) REFER
 ALTER TABLE `transaction` ADD CONSTRAINT `transactionUserFK` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 insert into `users` (fullname, email, password, city, state, zip) values
-('Brandon Derricho', 'bderricho@pocketbudget.com', 'derricho', 'Statesboro', 'GA', 30458),
-('Griffin Bryant', 'gbryant@pocketbudget.com', 'bryant', 'Statesboro', 'GA', 30458),
-('Dallas Martin', 'dmartin@pocketbudget.com', 'martin', 'Statesboro', 'GA', 30458),
-('Toby Burkey', 'tburkey@pocketbudget.com', 'burkey', 'Statesboro', 'GA', 30458);
+  ('Brandon Derricho', 'bderricho@pocketbudget.com', 'derricho', 'Statesboro', 'GA', 30458),
+  ('Griffin Bryant', 'gbryant@pocketbudget.com', 'bryant', 'Statesboro', 'GA', 30458),
+  ('Dallas Martin', 'dmartin@pocketbudget.com', 'martin', 'Statesboro', 'GA', 30458),
+  ('Toby Burkey', 'tburkey@pocketbudget.com', 'burkey', 'Statesboro', 'GA', 30458);
 
 insert into `budget` (user_id, amount_total, bills_percent, food_percent, gas_percent, savings_percent, fun_percent) values
-(1, 1000, 200, 200, 200, 200, 200),
-(2, 1000, 200, 200, 200, 200, 200),
-(3, 1000, 200, 200, 200, 200, 200),
-(4, 1000, 200, 200, 200, 200, 200);
+  (1, 1000, 200, 200, 200, 200, 200),
+  (2, 1000, 200, 200, 200, 200, 200),
+  (3, 1000, 200, 200, 200, 200, 200),
+  (4, 1000, 200, 200, 200, 200, 200);
+
+insert into `transaction` (user_id, category, amount, description) values
+  (2, 'Bills', 1450, 'Rent'),
+  (2, 'Bills', 450, 'Internet Bill'),
+  (2, 'Bills', 380, 'Phone Bill'),
+  (2, 'Bills', 120, 'Utilities'),
+  (2, 'Fun', 300, 'Bowling Shoes'),
+  (2, 'Food', 86, 'Walmart'),
+  (2, 'Food', 2.50, 'McDonalds'),
+  (2, 'Gas', 40, 'Gas'),
+  (2, 'Food', 2.50, 'McDonalds'),
+  (2, 'Savings', 250, 'Savings Deposit'),
+  (2, 'Food', 2.50, 'McDonalds'),
+  (2, 'Savings', 1, 'You never know when youre gonna need it'),
+  (2, 'Food', 2.50, 'McDonalds'),
+  (2, 'Food', 8.75, 'Panda Express'),
+  (2, 'Food', 2.50, 'McDonalds');
